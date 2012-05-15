c8b66a4dfdb179683eae323dcacd81e7e0522571
gtx1049 <gtx_122@126.com>
2012-05-16 02:09:07 +0800

    第一次

---
+++ DataBaseDesignCourse/DataManager/DataManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataBaseDesignCourse.Entitys;
+using System.Data.SqlClient;
+
+namespace DataBaseDesignCourse
+{
+
+    class DataManager
+    {
+        private SqlConnection connection;
+        private string connectionstring;
+        static private DataManager instance;
+
+ 
+
+        private DataManager()
+        {
+            //connection the data base from here
+            connectionstring = "Data Source=CHINA-2A781551C;Initial Catalog=PublicSafety;Integrated Security=True";
+            connection = new SqlConnection(connectionstring);
+        }
+
+        static public DataManager createInstance()
+        {
+            //singleton design pattern
+            if (instance == null)
+            {
+                instance = new DataManager();                
+            }
+            return instance;
+        }
+
+        public Entity FindbyPrimaryKey(string type, object primarykey)
+        {
+            Type table = Type.GetType(type);
+            object o = System.Activator.CreateInstance(table);
+            Entity theentity = (Entity)o;
+
+            string sql = "select * from " + theentity.getTableName() +
+             " where " + theentity.getPrimaryKeyName() + " = ";
+
+            if (theentity.getPrimaryKeyType())
+            {
+                Int32 pk = (Int32)primarykey;
+                try
+                {
+                    sql += pk.ToString();
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(sql, connection);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            else
+            {
+                string pk = (string)primarykey;
+                try
+                {
+                    sql += "'" + pk.ToString() + "'";
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(sql, connection);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    theentity.fillData(reader);
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            return theentity;
+        }
+
+        public bool Merge(Entity e)
+        {
+            try
+            {
+                string sql = "update " + e.getTableName() + " set ";
+                sql += e.getMergeSQL();
+                sql += " where " + e.getPrimaryKeyName() + " = " + e.getPrimaryKey();
+
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool Persist(Entity e)
+        {
+            try
+            {
+                string sql = "insert into " + e.getTableName() + " values";
+                sql += "(" + e.getPersistSQL() + ")";
+           
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+                connection.Close();
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ DataBaseDesignCourse/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DataBaseDesignCourse.Entitys;
+
+namespace DataBaseDesignCourse
+{
+	/// <summary>
+	/// Window1.xaml 的交互逻辑
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			this.InitializeComponent();
+            DataManager ins = DataManager.createInstance();
+            Citizen c = (Citizen)ins.FindbyPrimaryKey(Citizen.getClass(), "210703199104272017");
+            Citizen c2 = (Citizen)ins.FindbyPrimaryKey(Citizen.getClass(), "130824199005210010");
+            c.getID();
+            c2.getID();
+            // 在此点之下插入创建对象所需的代码。
+		}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+	}
+}
